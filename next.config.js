25092bcabd6edcd7324333d5265012026aaf5d81
shaiqur <shaiqur@iastate.edu>
2025-12-08 21:12:29 -0600

    DeployDNS

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/coaching-central-next",
-  assetPrefix: "/coaching-central-next/",
+  //basePath: "/coaching-central-next",
+  //assetPrefix: "/coaching-central-next/",
   images: {
     unoptimized: true,
   },
